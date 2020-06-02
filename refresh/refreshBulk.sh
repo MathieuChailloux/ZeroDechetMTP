065a0540c3ad36eb1d28618ba2fde152563f1af0
Mathieu Chailloux <mathieu@chailloux.org>
2020-06-02 16:18:25 +0200

    Adding bulkYes

--- refresh/refreshBulk.sh
+++ refresh/refreshBulk.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-curl -o bulk.json -g "https://www.overpass-api.de/api/interpreter?data=[out:json];(area[\"ISO3166-1\"~\"FR|MC|BE|CH\"];)->.fr;((node[\"bulk_purchase\"=\"only\"](area.fr);way[\"bulk_purchase\"=\"only\"](area.fr);););out%20center;"
+curl -o bulk.json -g "https://www.overpass-api.de/api/interpreter?data=[out:json];(area[\"ISO3166-1\"~\"FR|MC|BE|CH\"];)->.fr;((node[\"bulk_purchase\"~\"yes|only\"](area.fr);way[\"bulk_purchase\"~\"yes|only\"](area.fr);););out%20center;"
