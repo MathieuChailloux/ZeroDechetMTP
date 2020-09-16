1f04e0cc7a2a7980f6b44811410df126789b2964
Mathieu Chailloux <fdrmc@free.fr>
2020-09-16 21:35:57 +0200

    Refresh all OSM imports

--- refresh/refreshCompost.sh
+++ refresh/refreshCompost.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-curl -o compost.json -g "https://www.overpass-api.de/api/interpreter?data=[out:json][timeout:500][maxsize:20737418];(area[\"ISO3166-1\"~\"FR|MC|BE|CH\"];)->.fr;(node[\"recycling:organic\"~\"yes\"](area.fr);way[\"recycling:organic\"~\"yes\"](area.fr););out%20center;"
+curl -o compost.json -g "https://www.overpass-api.de/api/interpreter?data=[out:json][timeout:500][maxsize:20737418];(area[\"ISO3166-1\"~\"FR|MC|BE|CH|AD\"];)->.fr;(node[\"recycling:organic\"~\"yes\"](area.fr);way[\"recycling:organic\"~\"yes\"](area.fr););out%20center;"
